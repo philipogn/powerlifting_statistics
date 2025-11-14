e76318c727c1d47a86d47123f87f8965dbb78b2a
Phillip <philip.ogn@gmail.com>
2025-11-14 20:35:55 +0000

    refactor + training.py

--- src/data_cleaning.py
+++ src/data_cleaning.py
@@ -1,40 +1,51 @@
-'''
-Event: only SBD
-Equipment: Raw only
-Age: drop empty fields
-Division: Open, MR-O, FR-O only, as these are the standard
-TotalKg: drop empty fields (no DQs)
-Place: must be a number (no DQs)
-ParentFederation: only IPF
-Best3s: ensure all 3 lifts are successful (no DQs)
-'''
+import pandas as pd
 
-def data_cleaning(df):
-    data = df[
-        (df['Event'] == 'SBD') & 
-        (df['Equipment'] == 'Raw') &
-        (df['Age'].notna()) &
-        (df['Division'].isin(['Open', 'MR-O', 'FR-O'])) &
-        (df['TotalKg'].notna()) &
-        (df['Place'].str.isnumeric()) &
-        (df['ParentFederation'] == 'IPF') &
-        (df['Best3SquatKg'].notna()) &
-        (df['Best3BenchKg'].notna()) &
-        (df['Best3DeadliftKg'].notna()) 
-    ].copy()
+class DataCleaning():
+    def __init__(self):
+        pass
+    
+    '''
+    Event: only SBD
+    Equipment: Raw only
+    Age: drop empty fields
+    Division: Open, MR-O, FR-O only, as these are the standard
+    TotalKg: drop empty fields (no DQs)
+    Place: must be a number (no DQs)
+    ParentFederation: only IPF
+    Best3s: ensure all 3 lifts are successful (no DQs)
+    '''
 
-    essential_columns = [
-        'Name', 'Date', 'Sex', 'Age', 'BodyweightKg',
-        'Best3SquatKg', 'Best3BenchKg', 'Best3DeadliftKg', 
-        'TotalKg', 'Dots'
-    ]
+    def data_cleaning(df):
+        print('Started data cleaning...')
+        data = df[
+            (df['Event'] == 'SBD') & 
+            (df['Equipment'] == 'Raw') &
+            (df['Age'].notna()) &
+            (df['Division'].isin(['Open', 'MR-O', 'FR-O'])) &
+            (df['TotalKg'].notna()) &
+            (df['Place'].str.isnumeric()) &
+            (df['ParentFederation'] == 'IPF') &
+            (df['Best3SquatKg'].notna()) &
+            (df['Best3BenchKg'].notna()) &
+            (df['Best3DeadliftKg'].notna()) 
+        ].copy()
 
-    data = data[essential_columns].copy()
+        essential_columns = [
+            'Name', 'Date', 'Sex', 'Age', 'BodyweightKg',
+            'Best3SquatKg', 'Best3BenchKg', 'Best3DeadliftKg', 
+            'TotalKg', 'Dots'
+        ]
 
-    # Convert to datetime
-    data['Date'] = pd.to_datetime(data['Date'])
-    # Sort by lifter and date (important for feature engineering)
-    data = data.sort_values(['Name', 'Date']).reset_index(drop=True)
+        data = data[essential_columns].copy()
 
-    data.to_csv('../data/2-preprocessed/cleanIPF_minimal.csv', index=False)
-    return data
+        # Convert to datetime and sort by lifter and date (important for feature engineering)
+        data['Date'] = pd.to_datetime(data['Date'])
+        data = data.sort_values(['Name', 'Date']).reset_index(drop=True)
+
+        data.to_csv('data/2-preprocessed/cleanIPF_minimal.csv', index=False)
+        print('Successfully cleaned data and saved to "data/2-preprocessed/cleanIPF_minimal.csv"')
+        return data
+
+if __name__ == '__main__':
+    file_path = pd.read_csv('data/1-raw/openpowerlifting-2025-09-27-31932eca.csv')
+    DataCleaning.data_cleaning(file_path)
---
+++ src/training.py
@@ -0,0 +1,104 @@
+import pandas as pd
+from sklearn.linear_model import LinearRegression
+from sklearn.ensemble import RandomForestRegressor, GradientBoostingRegressor
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import r2_score, mean_squared_error, mean_absolute_error
+import numpy as np
+
+class TrainingPipeline():
+    def __init__(self, dataframe):
+        self.dataframe = dataframe
+        self.feature_columns = ['prev_squat', 'prev_bench', 'prev_deadlift', 'prev_total',
+                                'avg_squat', 'avg_bench', 'avg_deadlift', 'avg_total',
+                                'max_total_ever', 'min_total_ever', 
+                                'squat_gain_per_meet', 'bench_gain_per_meet', 'deadlift_gain_per_meet', 'total_gain_per_meet',
+                                'Age', 'BodyweightKg'
+                                ]
+        self.X_train = None
+        self.X_test = None
+        self.y_train = None
+        self.y_test = None
+        self.train_df = None
+        self.test_df = None
+        self.models = {}
+        self.predictions = {}
+        self.results = {}
+
+    def load_dataset(self):
+        pass
+
+
+    def prepare_dataset(self):
+        print('')
+        self.dataframe['SexEncoded'] = self.dataframe['Sex'].map({'M': 1, 'F': 0})
+        self.feature_columns.append('SexEncoded')
+        self.dataframe[self.feature_columns] = self.dataframe[self.feature_columns].fillna(0)
+
+        # Time-base train-test split
+        self.dataframe['Date'] = pd.to_datetime(self.dataframe['Date'])
+        self.dataframe = self.dataframe.sort_values('Date').reset_index(drop=True)
+
+        split_date = self.dataframe['Date'].quantile(0.8)
+        self.train_df = self.dataframe[self.dataframe['Date'] < split_date]
+        self.test_df = self.dataframe[self.dataframe['Date'] >= split_date]
+
+        print(f"Data split:")
+        print(f"Training date range: {self.train_df['Date'].min()} to {self.train_df['Date'].max()}")
+        print(f"Testing date range: {self.test_df['Date'].min()} to {self.test_df['Date'].max()}")
+        print(f"Training examples: {len(self.train_df)}")
+        print(f"Testing examples: {len(self.test_df)}")
+
+        self.X_train = self.train_df[self.feature_columns]
+        self.X_test = self.test_df[self.feature_columns]
+        self.y_train = self.train_df['TotalKg']
+        self.y_test = self.test_df['TotalKg']
+
+    def train_models(self):
+        lr = LinearRegression()
+        lr.fit(self.X_train, self.y_train)
+        lr_y_pred = lr.predict(self.X_test)
+        self.models['Linear Regression'] = lr
+        self.predictions['Linear Regression'] = lr_y_pred
+
+        rdr = RandomForestRegressor()
+        rdr.fit(self.X_train, self.y_train)
+        rdr_y_pred = rdr.predict(self.X_test)
+        self.models['Random Forest'] = rdr
+        self.predictions['Random Forest'] = rdr_y_pred
+
+        gbr = GradientBoostingRegressor()
+        gbr.fit(self.X_train, self.y_train)
+        gbr_y_pred = gbr.predict(self.X_test)
+        self.models['Gradient Boosting'] = gbr
+        self.predictions['Gradient Boosting'] = gbr_y_pred
+
+    # Evaluate models
+    def evaluate_models(self, y_true, y_pred, model_name):
+        mae = mean_absolute_error(y_true, y_pred)
+        rmse = np.sqrt(mean_squared_error(y_true, y_pred))
+        mse = mean_squared_error(y_true, y_pred)
+        r2 = r2_score(y_true, y_pred)
+
+        print(f'\n==={model_name}===')
+        print(f'Mean Absolute Error: {mae:.4f}')
+        print(f'Mean Squared Error: {mse:.4f}')
+        print(f'Root Mean Squared Error: {rmse:.4f}')
+        print(f'R2 Score: {r2:.4f}')
+
+        return {'MAE': mae, 'MSE': mse, 'RSME': rmse, 'R2': r2}
+
+    def run(self):
+        self.load_dataset()
+        self.prepare_dataset()
+        self.train_models()
+        for model_name, y_pred in self.predictions.items():
+            self.results[model_name] = self.evaluate_models(self.y_test, y_pred, model_name)
+
+        print('\n==Comparison==')
+        comparison = pd.DataFrame(self.results).T
+        print(comparison.round(2))
+
+if __name__ == '__main__':
+    df = pd.read_csv('data/3-features/engineered_features.csv')
+    train = TrainingPipeline(df)
+    train.run()
