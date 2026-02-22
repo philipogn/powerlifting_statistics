447f3c683dba333fa820f114b41b933f735b4059
Phillip <philip.ogn@gmail.com>
2026-02-22 17:52:21 +0000

    raw to train pipeline working

--- entrypoint/train.py
+++ entrypoint/train.py
@@ -13,14 +13,19 @@ from training import TrainingPipeline
 
 config = yaml.safe_load(open('config/prod.yaml'))
 
-def run_entire_pipeline(raw_data):
+def preprocess_step(raw_data_path):
     try:
+        raw_data = pd.read_csv(raw_data_path)
         preprocess = DataProcessor(
             save_path='data/2-preprocessed/openpowerlifting_preprocessed.csv',
             save_to_csv=True
         )
         preprocess.transform(raw_data)
+    except FileNotFoundError as e:
+        print(f'Error loading raw dataset: {e}')
 
+def training_step():
+    try:
         df = pd.read_csv('data/2-preprocessed/openpowerlifting_preprocessed.csv')
         features = FeatureEngineering(
             save_path='data/3-features/openpowerlifting_features.csv', 
@@ -28,10 +33,17 @@ def run_entire_pipeline(raw_data):
         )
         features.engineer_features(df)
 
-        train = TrainingPipeline.run()
+        df = pd.read_csv('data/3-features/openpowerlifting_features.csv')
+        train = TrainingPipeline()
         train.train_from_data(df)
-    except:
-        print('Error during training pipeline')
+    except FileNotFoundError as e:
+        print(f'Preprocessed dataset not found: {e}')
+
+# save model
+
+def run_entire_pipeline(raw_data_path):
+    preprocess_step(raw_data_path)
+    training_step()
 
 # def predict_on_unseen():
 #     test_df = pd.read_csv('data/3-features/Test_dataset.csv')
@@ -54,6 +66,5 @@ def run_entire_pipeline(raw_data):
 #         print(e)
 
 if __name__ == '__main__':
-    raw_data = pd.read_csv('data/1-raw/openpowerlifting-2025-09-27.csv')
-    run_entire_pipeline(raw_data)
+    run_entire_pipeline(raw_data_path='data/1-raw/openpowerlifting-2025-09-27.csv')
     # predict_on_unseen()
